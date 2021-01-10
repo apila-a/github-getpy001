44466b91f5e74203ba424fdd6d1e4e51679fb81c
APILA <997716784@qq.com>
2021-01-10 17:03:25 +0800

    Revert "第一次体检本项目"
    This reverts commit 0c43135e350ed326c1f1128e0ac63148610b6934.

--- my-tanhua/my-tanhua-server/src/main/java/com/tanhua/server/utils/NoAuthorization.java
+++
@@ -1,9 +0,0 @@
-package com.tanhua.server.utils;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented //标记为注解
-public @interface NoAuthorization {
-}
